078005db44e374ab53185527eaa7f8c341555d41
harrislloydlevy <harris.lloydlevy@gmail.com>
2021-10-28 13:30:40 +1100

    Helper functiosn find find combinations of numbers.

--- src/solvers.rs
+++ src/solvers.rs
@@ -45,15 +45,109 @@ pub fn normalise_boxes(mut boxes: Vec<&mut Box>) {
 	}
 }
 
+/**
+ * only_options
+ *
+ * Looks for combinations of boxes where there are only X boxes that can
+ * only be a set combination of X values, then remove that possible combination
+ * from all other boxes.
+ *
+ * e.g. if there is a set of 9 boxes, but 2 of them can *only* be 6 or 9, then
+ * logically none of the other boxes in the set can be 6 or 9, so remove them as
+ * options. This also applies for groups of 3 or 4.
+ *
+ * Groups of 5 or 6 are possible, but so rare and computationally expensive we don't bother. 
+pub fn only_options(mut boxes: Vec<&mut Box>) {
+  /*
+   * The logic we will follow for this function is as follows:
+   *  - Iterate over every number of factorials we'll look for 2, 3, and 4
+   *    - Then iterate over every possilbe combination of 1-9 of that number of factorials
+   *       - Then check how many boxes match that combination exactly
+   *       - And If that matches the number of factoritals then:
+   *         - remove that combination from all other boxes.
+   * 
+   * For convenience of checkign we'll primarially use bit patterns, and arrays of bit patterns.
+   */
+}
+ */
+
+
+/**
+ * For a given count of combinations provide an array of bit patterns that represent every
+ * combination of that number of values.
+fn combinations_maps(factors:u16) -> Vec<u16> {
+   assert!(factors > 1);
+   assert!(factors < 4);
+
+   let result = Vec::new();
+
+   result	
+}
+ */
+
+/**
+ * More generic combinations function used in combinations_maps that calls itself recursively
+ * to get all the combinartion bitmaps.
+ *
+ * Code not copied/pasted from rosetta-code, but algorithim and ideas from there.
+ *
+ * pool: The list of numbers left to choose from
+ * need: The number of combinations needed
+ * returns: A list of u16s, each one is a bitmap the number of needed values from the pool of possible values
+ *          e.g. 000011 would mean a combination of 1 and 2, 1011, a combiantions of 4, 2, and 1, 100001 = 6 and 1. 
+ */
+fn combo(pool: &[u16], k:u16) -> Vec<u16> {
+  println!("{:?} for {}", pool, k);
+  let mut result = Vec::new();
+
+  if k > (pool.len() as u16) {
+    println!("{} < {} - Terminating", k, pool.len());
+    return result;
+  }
+
+  for i in 0..pool.len() {
+    println!("{} of {} for {}", i, pool.len(), k);
+    if k == 1 {
+	  result.push(ON << pool[i]);
+	} else {
+      // Now we take the element we're up to in the pool of possible
+      // values (pool[i]), and find all the possible combinations
+      // that could start with that value, using the pool we haven't
+      // used yet, with k-1 iterations in. The value we're up to is then
+      // whacked into that bit pattern to make it a proper K number of 
+      // combinations.
+
+      // Base pattern showing the number we're starting with.
+      let base_bit_pattern = ON << pool[i];
+
+	  // Get all the results for one less number of combinations and
+      // not including the elements we've already used in this round.
+      // Magic of recusrion makes this work as it bottoms out to individual
+      // numbers above.
+      //
+      // I FUCKING LOVE RECURSION!
+      let subresults = combo(&pool[i+1..],k-1);
+
+      for element in subresults {
+		// Now add on all those combinations with the starting number
+        // we already have.
+        result.push(base_bit_pattern | element);
+      }
+	}
+  }
+
+  result
+}
+
 #[cfg(test)]
 mod tests {
     // Inherit everything from up a level so we can run functions from there.
     use super::*;
 	use crate::sk_box::BLANK_BOX;
 
-	#[test]
 	// Check that we can run a normalisation over 9 boxes that may not all
 	// come from the same sudoku cell.
+	#[test]
 	fn test_last_value_line() {
 		let mut line = Vec::new();
 
@@ -83,5 +177,35 @@ mod tests {
 		normalise_boxes(line);
 
 		assert!(box1.value == Some(1));
+	}
+
+    #[test]
+	fn test_simple_combo() {
+        let result = combo(&[1, 2, 3, 4], 2);
+		// Combos we want to see are:
+        // 1,2 / 1,3 / 1,4 / 2,3 / 2,4 / 3,4
+		for element in result.iter() {
+			println!("{:#018b}, ", element);
+		}
+		assert_eq!(result.len(), 6);
+		assert!(result.contains(&(ON << 1 | ON << 2)));		
+		assert!(result.contains(&(ON << 1 | ON << 3)));		
+		assert!(result.contains(&(ON << 1 | ON << 4)));		
+		assert!(result.contains(&(ON << 2 | ON << 3)));		
+		assert!(result.contains(&(ON << 2 | ON << 4)));		
+		assert!(result.contains(&(ON << 3 | ON << 4)));		
+	}
+
+	#[test]
+	fn test_complex_combo() {
+        let result = combo(&[1, 2, 3, 4, 5, 6, 7, 8, 9], 4);
+		// Combos we want to see are:
+        // 1,2 / 1,3 / 1,4 / 2,3 / 2,4 / 3,4
+		for element in result.iter() {
+			println!("{:#018b}, ", element);
+		}
+		// Not checking everyone. There are 126 possible combinations of 4 objects from
+        // a pool of 9 so justt check that.
+		assert_eq!(result.len(), 126);
 	}
 }
